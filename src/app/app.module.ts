283cc681a660731635f7179d362d1b35bed1a802
Programmer7Zilla <djkoleh@outlook.com>
2021-05-28 21:38:41 +0530

    Check the version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponet } from './server/server.componet';
+import { from } from 'rxjs';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './successAlert/success.component';
+import { WarningAlertComponent } from './warningAlert/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponet,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
